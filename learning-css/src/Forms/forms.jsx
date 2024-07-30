26c7c1c156b244a43e3c4fc2cb6320a4087d2d8c
vikash-choudhary-tech <77918018+vikash-choudhary-tech@users.noreply.github.com>
2024-07-30 22:24:09 +0530

    CSS for forms completed

---
+++ learning-css/src/Forms/forms.jsx
@@ -0,0 +1,42 @@
+import "./forms.css";
+
+const Forms = () => <>
+
+    <div className="form-container">
+        <form>
+        <div className="form-header">
+            Welcome To CSS. It's a Fascinating World...
+        </div>
+        <div className="form-group">
+            <label>Name :</label>
+            <input type="text"/>
+        </div>
+        <div className="form-group">
+            <label>Age :</label>
+            <input type="number"/>
+        </div>
+        <div className="form-group">
+        <label>Gender :</label>
+            <div className="form-group-radio">
+       
+            <input type="radio" value={'male'} name="gender" defaultChecked="false"/>
+            Male
+            <input type="radio" value={'Female'} name="gender" defaultChecked="false"/>
+            Female
+        </div>
+        </div>
+        <div className="form-group">
+        <label className="checkbox">Terms and Condition Privacy :</label>
+            <div className="form-group-checkbox">
+                <input type="checkbox" value={'I Agree'}/>
+                I Agree
+            </div>  
+        </div>
+        </form>
+    </div>  
+
+
+
+</>
+
+export default Forms;
